859cac22f2f902cdf6195c309b7e59fd1cb5af7c
KUCZYN\kaczk <kaczksa>
2020-01-22 20:51:56 +0100

    dupa1

--- TestownikConsoleApp/QuestionParser.cs
+++ TestownikConsoleApp/QuestionParser.cs
@@ -10,17 +10,44 @@ namespace TestownikConsoleApp
     {
         public List<QuestionDto> ReadQuestions(string path)
         {
-            var directory = new DirectoryInfo(path);
-            var questionFiles = directory.GetFiles("*.txt");
+            var questionFiles = Directory.GetFiles(path);
             List<QuestionDto> questions = new List<QuestionDto>();
             foreach (var questionFile in questionFiles)
             {
-                using (StreamReader sr = File.OpenText(questionFile.))
+                string[] lines = File.ReadAllLines(questionFile);
+                if (lines.Length <= 4)
                 {
-                    var lines = sr.ReadToEnd()
+                    Console.WriteLine("Niepoprawna ilość odpowiedzi");
+                    continue;
                 }
-            }
 
+                var question = new QuestionDto();
+                question.Question = lines[1];
+
+                var answersPointers = lines[0].Split("");
+                var answerLines = lines.Skip(2).ToArray();
+
+                if (answersPointers.Length != answerLines.Length)
+                {
+                    Console.WriteLine("Niezgodna ilośc opdowiedzi");
+                    continue;
+                }
+
+                question.Answers = new List<AnswerDto>();
+                for (int i = 0; i < answerLines.Length; i++)
+                {
+                    string answerLine = answerLines[i];
+                    string result = answersPointers[i];
+                    var answer = new AnswerDto();
+                    answer.Answer = answerLine;
+                    answer.IsCorrect = result == "1";
+                    question.Answers.Add(answer);
+                }
+
+                questions.Add(question);
+
+            }
+            return questions;
         }
     }
 }
